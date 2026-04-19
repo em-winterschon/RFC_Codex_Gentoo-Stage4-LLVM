9b5e7162aacdafceaa264191cc9217d987fd5d2f
Eva (Annaliese) Winterschön <7950191+em-winterschon@users.noreply.github.com>
2026-04-19 00:30:16 +0000

    Run VFIO PCI remap shell tests

--- tests/shell/run-tests.sh
+++ tests/shell/run-tests.sh
@@ -16,5 +16,6 @@ done
 
 "${SCRIPT_DIR}/test_generate_ansible_python_setup.sh"
 "${SCRIPT_DIR}/test_qemu_launch_minimal_vm.sh"
+"${SCRIPT_DIR}/test_qemu_pci_remap.sh"
 
 printf 'PASS: %s\n' "$(basename "$0")"
