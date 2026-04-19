9823848a86a5a215e4589fec95e0e71c5d4e2052
Eva (Annaliese) Winterschön <7950191+em-winterschon@users.noreply.github.com>
2026-04-19 09:12:28 -0700

    Add optional virt-viewer and libvirt host packages

--- gentoo-virt-qemu/gentoo-install-qemu-edk2.sh
+++ gentoo-virt-qemu/gentoo-install-qemu-edk2.sh
@@ -1,4 +1,21 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-emerge sys-firmware/edk2-bin app-emulation/qemu net-dialup/minicom
+INSTALL_VIRT_VIEWER="${INSTALL_VIRT_VIEWER:-1}"
+INSTALL_LIBVIRT="${INSTALL_LIBVIRT:-0}"
+
+packages=(
+  sys-firmware/edk2-bin
+  app-emulation/qemu
+  net-dialup/minicom
+)
+
+if [[ "${INSTALL_VIRT_VIEWER}" == '1' ]]; then
+  packages+=( app-emulation/virt-viewer )
+fi
+
+if [[ "${INSTALL_LIBVIRT}" == '1' ]]; then
+  packages+=( app-emulation/libvirt )
+fi
+
+emerge "${packages[@]}"
