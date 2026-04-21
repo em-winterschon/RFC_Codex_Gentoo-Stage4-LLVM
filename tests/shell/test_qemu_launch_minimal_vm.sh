6372eea7545600f9b62c61cbc3b258715a101bf3
Eva (Annaliese) Winterschön <7950191+em-winterschon@users.noreply.github.com>
2026-04-21 16:48:14 -0700

    Avoid shell profile noise in minimal launcher test

--- tests/shell/test_qemu_launch_minimal_vm.sh
+++ tests/shell/test_qemu_launch_minimal_vm.sh
@@ -297,7 +297,7 @@ test_build_qemu_cmd_uses_host_disks_and_virtio_net() {
 test_blank_env_overrides_defaults_in_fresh_process() {
   local output
 
-  output="$(bash -lc 'PCI_NETWK=; source "$1"; printf "%s\n" "$PCI_NETWK"' _ "${LAUNCH_SCRIPT}")"
+  output="$(bash --noprofile --norc -c 'PCI_NETWK=; source "$1"; printf "%s\n" "$PCI_NETWK"' _ "${LAUNCH_SCRIPT}")"
   assert_equals '' "${output}"
 }
 
